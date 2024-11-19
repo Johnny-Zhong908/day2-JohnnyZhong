01ca925e6abd163e315b5e3795eb3a5ff7698177
My Name <myEmail@example.com>
2024-11-19 18:16:38 +0800

    Using enumeration instead of switch case for refactoring optimization

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,7 +1,40 @@
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MarsRover {
+    private enum Direction {
+        N(0, 1),
+        E(1, 0),
+        S(0, -1),
+        W(-1, 0);
+
+        private final int dx;
+        private final int dy;
+
+        Direction(int dx, int dy) {
+            this.dx = dx;
+            this.dy = dy;
+        }
+    }
+
+    private static final Map<MarsRover.Direction, MarsRover.Direction> LEFT_TURNS = new HashMap<>();
+    private static final Map<MarsRover.Direction, MarsRover.Direction> RIGHT_TURNS = new HashMap<>();
+
+    static {
+
+        LEFT_TURNS.put(MarsRover.Direction.N, MarsRover.Direction.W);
+        LEFT_TURNS.put(MarsRover.Direction.W, MarsRover.Direction.S);
+        LEFT_TURNS.put(MarsRover.Direction.S, MarsRover.Direction.E);
+        LEFT_TURNS.put(MarsRover.Direction.E, MarsRover.Direction.N);
+
+        RIGHT_TURNS.put(MarsRover.Direction.N, MarsRover.Direction.E);
+        RIGHT_TURNS.put(MarsRover.Direction.E, MarsRover.Direction.S);
+        RIGHT_TURNS.put(MarsRover.Direction.S, MarsRover.Direction.W);
+        RIGHT_TURNS.put(MarsRover.Direction.W, MarsRover.Direction.N);
+    }
+
     public static void main(String[] args) {
         char Direction='N';
         int [] sites={0,0};
